cec61da054a6cbb7f0e36cfa6e64dda0ff26d7f2
Mike Dyer <mike.dyer@mathembedded.com>
2012-01-28 17:56:18 +0000

    Add build stub for gstreamer

---
+++ xbmc/cores/gstPlayer/gstPlayer.cpp
@@ -0,0 +1,10 @@
+/*
+ * gstPlayer.cpp
+ *
+ *  Created on: Jan 28, 2012
+ *      Author: mike
+ */
+
+#include "gstPlayer.h"
+
+
---
+++ xbmc/cores/gstPlayer/gstPlayer.h
@@ -0,0 +1,14 @@
+/*
+ * gstPlayer.h
+ *
+ *  Created on: Jan 28, 2012
+ *      Author: mike
+ */
+
+#ifndef GSTPLAYER_H_
+#define GSTPLAYER_H_
+
+
+
+
+#endif /* GSTPLAYER_H_ */
